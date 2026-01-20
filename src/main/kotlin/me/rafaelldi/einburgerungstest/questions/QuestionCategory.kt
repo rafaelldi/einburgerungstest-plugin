e382141672b50f694dd9fe968bdf1986b6fc1c04
Rival Abdrakhmanov <github.rectangle195@passmail.net>
2026-01-20 18:57:10 +0100

    Add question categories

---
+++ src/main/kotlin/me/rafaelldi/einburgerungstest/questions/QuestionCategory.kt
@@ -0,0 +1,29 @@
+package me.rafaelldi.einburgerungstest.questions
+
+internal enum class QuestionCategory(category: String) {
+    BildungUndArbeit("Bildung und Arbeit"),
+    BundUndLaender("Bund und Länder"),
+    EuropaUndWelt("Europa und Welt"),
+    Geschichte("Geschichte"),
+    GesellschaftUndFamilie("Gesellschaft und Familie"),
+    Politik("Politik"),
+    Recht("Recht"),
+    ReligionUndKultur("Religion und Kultur"),
+    Staat("Staat"),
+    Wirtschaft("Wirtschaft"),
+    BadenWuerttemberg("Baden-Württemberg"),
+    Bayern("Bayern"),
+    Berlin("Berlin"),
+    Brandenburg("Brandenburg"),
+    Bremen("Bremen"),
+    Hamburg("Hamburg"),
+    Hessen("Hessen"),
+    MecklenburgVorpommern("Mecklenburg-Vorpommern"),
+    Niedersachsen("Niedersachsen"),
+    NordrheinWestfalen("Nordrhein-Westfalen"),
+    RheinlandPfalz("Rheinland-Pfalz"),
+    Sachsen("Sachsen"),
+    SachsenAnhalt("Sachsen-Anhalt"),
+    SchleswigHolstein("Schleswig-Holstein"),
+    Thueringen("Thüringen")
+}
